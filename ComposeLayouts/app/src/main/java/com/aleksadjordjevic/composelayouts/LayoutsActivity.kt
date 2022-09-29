acf179e935369101fe8fcb22fe2ba20990a238b3
Aleksa Djordjevic <62656452+AleksaDjordjevic97@users.noreply.github.com>
2022-09-29 22:41:38 +0200

    Created the Home Screen with a button that sends to the Layouts Activity

---
+++ ComposeLayouts/app/src/main/java/com/aleksadjordjevic/composelayouts/LayoutsActivity.kt
@@ -0,0 +1,14 @@
+package com.aleksadjordjevic.composelayouts
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class LayoutsActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+}
